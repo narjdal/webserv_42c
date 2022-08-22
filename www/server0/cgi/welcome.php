668bde29d376aec7c10380e624c0e9606d85c399
sboof911 <amaach@student.1337.ma>
2022-08-22 01:02:18 +0100

    l

--- www/server0/cgi/welcome.php
+++ www/server0/cgi/welcome.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome from PHP: <?php echo $_POST['name']; ?><br>
+Your email address is: <?php echo $_POST['email']; ?>
 
 </body>
 </html>
